a70fba9426ea818d2400e7f8db6911dd7006ae99
Michael Snoyman <michael@snoyman.com>
2009-12-11 13:19:20 +0200

    Added Data.Object.Html with main conversions.
    Has support for raw output, conversion to JSON, and use in
    HStringTemplate.

--- runtests.hs
+++ runtests.hs
@@ -3,10 +3,12 @@ import Test.Framework (defaultMain)
 import qualified Yesod.Response
 import qualified Yesod.Utils
 import qualified Yesod.Resource
+import qualified Data.Object.Html
 
 main :: IO ()
 main = defaultMain
     [ Yesod.Response.testSuite
     , Yesod.Utils.testSuite
     , Yesod.Resource.testSuite
+    , Data.Object.Html.testSuite
     ]
