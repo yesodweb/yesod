57e4bef9574dec3c2544d9bdd4f22e2a042c95de
Michael Snoyman <michael@snoyman.com>
2010-05-02 07:56:16 +0300

    Removed old tests and yesod CLI

--- runtests.hs
+++ runtests.hs
@@ -1,15 +1,10 @@
 import Test.Framework (defaultMain)
 
--- FIXME import qualified Test.Errors
--- FIXME import qualified Test.QuasiResource
 import qualified Web.Mime
 import qualified Yesod.Json
 
 main :: IO ()
 main = defaultMain
-    [
-      Web.Mime.testSuite
+    [ Web.Mime.testSuite
     , Yesod.Json.testSuite
-    -- FIXME , Test.Errors.testSuite
-    -- FIXME, Test.QuasiResource.testSuite
     ]
