d37fce19654f7cef1fe0b24ab6ee5e81479eec9f
Michael Snoyman <michael@snoyman.com>
2010-12-21 00:29:25 +0200

    Initial code pull from yesod

---
+++ Yesod/Persist.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
+module Yesod.Persist
+    ( YesodPersist (..)
+    , get404
+    , getBy404
+    , module Database.Persist
+    ) where
+
+import Database.Persist
+import Control.Monad.Trans.Class (MonadTrans (..))
+import Control.Failure (Failure)
+
+import Yesod
+
+class YesodPersist y where
+    type YesodDB y :: (* -> *) -> * -> *
+    runDB :: YesodDB y (GHandler sub y) a -> GHandler sub y a
+
+-- Get the given entity by ID, or return a 404 not found if it doesn't exist.
+get404 :: (PersistBackend (t m), PersistEntity val, Monad (t m),
+           Failure ErrorResponse m, MonadTrans t)
+       => Key val -> t m val
+get404 key = do
+    mres <- get key
+    case mres of
+        Nothing -> lift notFound
+        Just res -> return res
+
+-- Get the given entity by unique key, or return a 404 not found if it doesn't
+-- exist.
+getBy404 :: (PersistBackend (t m), PersistEntity val, Monad (t m),
+             Failure ErrorResponse m, MonadTrans t)
+         => Unique val -> t m (Key val, val)
+getBy404 key = do
+    mres <- getBy key
+    case mres of
+        Nothing -> lift notFound
+        Just res -> return res
