be89f673ae3a5788d3d9ec1e187e6c79e0374af4
patrick brisbin <pbrisbin@gmail.com>
2011-01-18 10:40:15 -0500

    atom namespace is required for self link, which is recommended always

--- Yesod/Helpers/RssFeed.hs
+++ Yesod/Helpers/RssFeed.hs
@@ -38,7 +38,8 @@ template arg =
 #else
     [$xhamlet|
 #endif
-    %rss!version="2.0"
+    \<?xml version="1.0" encoding="utf-8"?> 
+    %rss!version="2.0"!xmlns:atom="http://www.w3.org/2005/Atom"
 
         %channel
             %atom:link!href=@feedLinkSelf.arg@!rel="self"!type=$typeRss$
