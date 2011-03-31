fe38853ff02c8ae543ce183d230bf0273de3e504
Michael Snoyman <michael@snoyman.com>
2011-03-31 23:58:40 +0200

    Mini scaffolded site

--- full-test.sh
+++ full-test.sh
@@ -3,5 +3,5 @@
 cabal clean && cabal install && rm -rf foobar && \
     yesod < input-sqlite && cd foobar && cabal install && cd .. && \
     yesod < input-postgres && cd foobar && cabal install && cd .. && \
-    # yesod < input-mini && cd foobar && cabal install && cd .. && \
+    yesod < input-mini && cd foobar && cabal install && cd .. && \
     rm -rf foobar
--- scaffold.hs
+++ scaffold.hs
@@ -10,6 +10,7 @@ import Control.Applicative ((<$>))
 import qualified Data.ByteString.Lazy as L
 import qualified Data.Text.Lazy as LT
 import qualified Data.Text.Lazy.Encoding as LT
+import Control.Monad (when)
 
 qq :: String
 #if GHC7
@@ -56,13 +57,14 @@ main = do
 
     putStr $(codegen "database")
     hFlush stdout
-    backendS <- prompt $ flip elem ["s", "p"]
+    backendS <- prompt $ flip elem ["s", "p", "m"]
     let pconn1 = $(codegen "pconn1")
     let pconn2 = $(codegen "pconn2")
-    let (lower, upper, connstr1, connstr2) =
+    let (lower, upper, connstr1, connstr2, importDB) =
             case backendS of
-                "s" -> ("sqlite", "Sqlite", "debug.db3", "production.db3")
-                "p" -> ("postgresql", "Postgresql", pconn1, pconn2)
+                "s" -> ("sqlite", "Sqlite", "debug.db3", "production.db3", "import Database.Persist.Sqlite\n")
+                "p" -> ("postgresql", "Postgresql", pconn1, pconn2, "import Database.Persist.Postgresql\n")
+                "m" -> ("FIXME lower", "FIXME upper", "FIXME connstr1", "FIXME connstr2", "")
                 _ -> error $ "Invalid backend: " ++ backendS
 
     putStrLn "That's it! I'm creating your files now..."
@@ -80,23 +82,23 @@ main = do
     mkDir "cassius"
     mkDir "julius"
     mkDir "static"
-    
+
     writeFile' "test.hs" $(codegen "test_hs")
     writeFile' "production.hs" $(codegen "production_hs")
     writeFile' "devel-server.hs" $(codegen "devel-server_hs")
-    writeFile' (project ++ ".cabal") $(codegen "cabal")
+    writeFile' (project ++ ".cabal") $ if backendS == "m" then $(codegen "mini-cabal") else $(codegen "cabal")
     writeFile' "LICENSE" $(codegen "LICENSE")
-    writeFile' (sitearg ++ ".hs") $(codegen "sitearg_hs")
-    writeFile' "Controller.hs" $(codegen "Controller_hs")
-    writeFile' "Handler/Root.hs" $(codegen "Root_hs")
-    writeFile' "Model.hs" $(codegen "Model_hs")
-    writeFile' "Settings.hs" $(codegen "Settings_hs")
+    writeFile' (sitearg ++ ".hs") $ if backendS == "m" then $(codegen "mini-sitearg_hs") else $(codegen "sitearg_hs")
+    writeFile' "Controller.hs" $ if backendS == "m" then $(codegen "mini-Controller_hs") else $(codegen "Controller_hs")
+    writeFile' "Handler/Root.hs" $ if backendS == "m" then $(codegen "mini-Root_hs") else $(codegen "Root_hs")
+    when (backendS /= "m") $ writeFile' "Model.hs" $(codegen "Model_hs")
+    writeFile' "Settings.hs" $ if backendS == "m" then $(codegen "mini-Settings_hs") else $(codegen "Settings_hs")
     writeFile' "StaticFiles.hs" $(codegen "StaticFiles_hs")
     writeFile' "cassius/default-layout.cassius"
         $(codegen "default-layout_cassius")
     writeFile' "hamlet/default-layout.hamlet"
         $(codegen "default-layout_hamlet")
-    writeFile' "hamlet/homepage.hamlet" $(codegen "homepage_hamlet")
+    writeFile' "hamlet/homepage.hamlet" $ if backendS == "m" then $(codegen "mini-homepage_hamlet") else $(codegen "homepage_hamlet")
     writeFile' "cassius/homepage.cassius" $(codegen "homepage_cassius")
     writeFile' "julius/homepage.julius" $(codegen "homepage_julius")
   
