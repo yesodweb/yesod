66ff0968985fef80ca3caf5cf16ab81e82405057
Michael Snoyman <michael@snoyman.com>
2011-03-31 22:56:34 +0200

    Version bumps

---
+++ full-test.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cabal clean && cabal install && rm -rf foobar && \
+    yesod < input-sqlite && cd foobar && cabal install && cd .. && \
+    yesod < input-postgres && cd foobar && cabal install && cd .. && \
+    # yesod < input-mini && cd foobar && cabal install && cd .. && \
+    rm -rf foobar
