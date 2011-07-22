5cb8e4a60598e07fce3577620fec2205570f4116
Michael Snoyman <michael@snoyman.com>
2011-07-22 09:08:05 +0300

    Added install-all.sh

---
+++ install-all.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+PACKAGES="yesod-core yesod-json yesod-static yesod-persistent yesod-newsfeed yesod-form yesod-auth yesod-sitemap yesod yesod-examples"
+CABAL=cabal
+
+for package in $PACKAGES
+do
+    echo Installing $package
+    cd $package
+    $CABAL configure --enable-tests && $CABAL build && $CABAL test && ./Setup.lhs install || exit
+    cd ..
+done
