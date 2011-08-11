ee031c2ad71ba093bbe0660c67824003b2e63a2d
Greg Weber <greg@gregweber.info>
2011-08-11 09:58:26 -0700

    update install-all.sh for testing dependencies

--- install-all.sh
+++ install-all.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-PACKAGES="yesod-core yesod-json yesod-static yesod-persistent yesod-newsfeed yesod-form yesod-auth yesod-sitemap yesod"
 CABAL=cabal
 
+PACKAGES="yesod-core yesod-json yesod-static yesod-persistent yesod-newsfeed yesod-form yesod-auth yesod-sitemap yesod"
 for package in $PACKAGES
 do
     echo Installing $package
     cd $package
     ($CABAL configure --enable-tests ||
-      ($CABAL update && $CABAL install --only-dependencies && $CABAL configure --enable-tests)
+      ($CABAL update && $CABAL install HUnit QuickCheck hspec && $CABAL install --only-dependencies && $CABAL configure --enable-tests)
     ) && $CABAL build && $CABAL test && ./Setup.lhs install || exit
     cd ..
 done
