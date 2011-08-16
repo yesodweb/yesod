971ab6ec74ac1df2020dcc41c1b685cf840c45ac
Greg Weber <greg@gregweber.info>
2011-08-16 14:34:57 -0700

    do a cabal update just once
    previously was doing it lazily, but multiple times
    also install testing dependencies right away

--- install-all.sh
+++ install-all.sh
@@ -2,13 +2,17 @@
 
 CABAL=cabal
 
+$CABAL update
+# install testing dependencies
+$CABAL install HUnit QuickCheck hspec
+
 PACKAGES="yesod-core yesod-json yesod-static yesod-persistent yesod-newsfeed yesod-form yesod-auth yesod-sitemap yesod"
 for package in $PACKAGES
 do
     echo Installing $package
     cd $package
     ($CABAL configure --enable-tests ||
-      ($CABAL update && $CABAL install HUnit QuickCheck hspec && $CABAL install --only-dependencies && $CABAL configure --enable-tests)
+      ($CABAL install --only-dependencies && $CABAL configure --enable-tests)
     ) && $CABAL build && $CABAL test && ./Setup.lhs install || exit
     cd ..
 done
