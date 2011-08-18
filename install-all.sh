02b00d91ba5248f7135131c1bf1d12b535bca25d
Michael Snoyman <michael@snoyman.com>
2011-08-19 00:54:51 +0300

    install-all.sh: revert changes

--- install-all.sh
+++ install-all.sh
@@ -10,6 +10,8 @@ for package in $PACKAGES
 do
     echo Installing $package
     cd $package
-    $CABAL configure --enable-tests && $CABAL build && $CABAL test && ./Setup.lhs install || exit 1
+    ($CABAL configure --enable-tests ||
+        ($CABAL install --only-dependencies --enable-tests && $CABAL configure --enable-tests)
+    ) && $CABAL build && $CABAL test && ./Setup.lhs install || exit 1
     cd ..
 done
