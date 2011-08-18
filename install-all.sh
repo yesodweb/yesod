11232246eddb118ce823cc8ff93e605ae5e0f65d
Greg Weber <greg@gregweber.info>
2011-08-18 10:18:36 -0700

    add error code for exit

--- install-all.sh
+++ install-all.sh
@@ -10,6 +10,6 @@ for package in $PACKAGES
 do
     echo Installing $package
     cd $package
-    $CABAL configure --enable-tests && $CABAL build && $CABAL test && ./Setup.lhs install || exit
+    $CABAL configure --enable-tests && $CABAL build && $CABAL test && ./Setup.lhs install || exit 1
     cd ..
 done
