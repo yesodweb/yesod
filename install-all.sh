642fc472784380b81bdbec465bde2d62cfd967e7
Greg Weber <greg@gregweber.info>
2011-08-20 10:54:30 -0700

    tighten up hspec version

--- install-all.sh
+++ install-all.sh
@@ -3,7 +3,7 @@
 CABAL=cabal
 
 # install testing dependencies
-$CABAL install HUnit QuickCheck hspec shelltestrunner
+$CABAL install HUnit QuickCheck 'hspec >= 0.6.1 && < 0.7' shelltestrunner || exit 1
 
 PACKAGES="yesod-core yesod-json yesod-static yesod-persistent yesod-newsfeed yesod-form yesod-auth yesod-sitemap yesod"
 for package in $PACKAGES
