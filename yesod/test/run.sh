3ec880d6726e24f9069c7fd699c2f4b17639d2bc
Greg Weber <greg@gregweber.info>
2011-08-23 08:08:42 -0700

    exit status for test script

--- yesod/test/run.sh
+++ yesod/test/run.sh
@@ -10,8 +10,8 @@ cabal clean && cabal install && cabal sdist
 # this for loop only operates on 1 file (as per tail -1)
 for f in $(ls -1rt dist/*.tar.gz | tail -1)
 do
-  tar -xzvf $f && cd `basename $f .tar.gz`
-  shelltest ../test/scaffold.shelltest --color --diff $@ -- --hide-successes
+  tar -xzvf $f && cd `basename $f .tar.gz` || exit 1
+  shelltest ../test/scaffold.shelltest --color --diff $@ -- --hide-successes || exit 1
   cd ..
   rm -r `basename $f .tar.gz`
 done
