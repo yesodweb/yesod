8c30ebe3c01687f27ec814aac82f997e1429c173
Greg Weber <greg@gregweber.info>
2011-09-05 23:11:14 -0700

    fix shell equality

--- install-all.sh
+++ install-all.sh
@@ -30,7 +30,7 @@ for pkg in "${pkgs[@]}"; do
   (
     cd "./$pkg"
 
-    if [ $1 == "--clean" ]; then
+    if [ "$1" = "--clean" ]; then
       $CABAL clean
     fi
 
@@ -45,7 +45,7 @@ for pkg in "${pkgs[@]}"; do
     $CABAL test
 
     $CABAL check
-    if [ $1 != "--fast" ]; then
+    if [ "$1" != "--fast" ]; then
       $CABAL haddock --executables
     fi
     ./Setup.lhs install
