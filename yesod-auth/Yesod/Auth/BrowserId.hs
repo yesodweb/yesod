b00fd948bd7d8589f1343685de5e859810fc3aa2
Michael Snoyman <michael@snoyman.com>
2011-09-11 08:52:23 +0300

    authBrowserId'

--- yesod-auth/Yesod/Auth/BrowserId.hs
+++ yesod-auth/Yesod/Auth/BrowserId.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Yesod.Auth.BrowserId
     ( authBrowserId
+    , authBrowserId'
     ) where
 
 import Yesod.Auth
@@ -11,6 +12,8 @@ import Data.Text (Text)
 import Yesod.Core
 import Text.Hamlet (hamlet)
 import Control.Monad.IO.Class (liftIO)
+import qualified Data.Text as T
+import Data.Maybe (fromMaybe)
 
 #include "qq.h"
 
@@ -46,3 +49,33 @@ authBrowserId audience = AuthPlugin
         <img src="https://browserid.org/i/sign_in_green.png">
 |]
     }
+
+authBrowserId' :: YesodAuth m => AuthPlugin m
+authBrowserId' = AuthPlugin
+    { apName = pid
+    , apDispatch = \m ps ->
+        case (m, ps) of
+            ("GET", [assertion]) -> do
+                tm <- getRouteToMaster
+                r <- getUrlRender
+                let audience = T.takeWhile (/= '/') $ stripScheme $ r $ tm LoginR
+                memail <- liftIO $ checkAssertion audience assertion
+                case memail of
+                    Nothing -> error "Invalid assertion"
+                    Just email -> setCreds True Creds
+                        { credsPlugin = pid
+                        , credsIdent = email
+                        , credsExtra = []
+                        }
+            (_, []) -> badMethod
+            _ -> notFound
+    , apLogin = \toMaster -> do
+        addScriptRemote browserIdJs
+        addHamlet [QQ(hamlet)|
+<p>
+    <a href="javascript:navigator.id.getVerifiedEmail(function(a){if(a)document.location='@{toMaster complete}/'+a});">
+        <img src="https://browserid.org/i/sign_in_green.png">
+|]
+    }
+  where
+    stripScheme t = fromMaybe t $ T.stripPrefix "//" $ snd $ T.breakOn "//" t
