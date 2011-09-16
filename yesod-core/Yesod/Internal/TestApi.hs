f8af5a8fb96ef7b6a6c9dd777cf5bf4f0a3749a1
Björn Buckwalter <bjorn@buckwalter.se>
2011-09-17 01:48:02 +0800

    TestApi: improve docs and remove Request (..).

--- yesod-core/Yesod/Internal/TestApi.hs
+++ yesod-core/Yesod/Internal/TestApi.hs
@@ -1,10 +1,11 @@
 --
 -- | WARNING: This module exposes internal interfaces solely for the
--- purpose of facilitating unit testing with cabal install. Library
--- users should not import this module.
+-- purpose of facilitating cabal-driven testing of said interfaces.
+-- This module is NOT part of the public Yesod API and should NOT be
+-- imported by library users.
 --
 module Yesod.Internal.TestApi
-  ( Request (..), randomString, parseWaiRequest'
+  ( randomString, parseWaiRequest'
   ) where
 
-import Yesod.Internal.Request (Request (..), randomString, parseWaiRequest')
+import Yesod.Internal.Request (randomString, parseWaiRequest')
