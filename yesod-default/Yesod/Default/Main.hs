33e0f37400ead55119fe4e4d995add1ca2812bd1
patrick brisbin <pbrisbin@gmail.com>
2011-09-22 22:38:51 -0400

    Add defaultRunner (not scaffold not updated)

--- yesod-default/Yesod/Default/Main.hs
+++ yesod-default/Yesod/Default/Main.hs
@@ -1,15 +1,24 @@
+{-# LANGUAGE CPP                #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 module Yesod.Default.Main
     ( defaultMain
+    , defaultRunner
     , defaultDevelApp
     , defaultDevelAppWith
     ) where
 
+import Yesod.Core
 import Yesod.Default.Config
 import Yesod.Logger (Logger, makeLogger, logString, logLazyText, flushLogger)
 import Network.Wai (Application)
 import Network.Wai.Handler.Warp (run)
 import Network.Wai.Middleware.Debug (debugHandle)
+
+#ifndef WINDOWS
+import qualified System.Posix.Signals as Signal
+import Control.Concurrent (forkIO, killThread)
+import Control.Concurrent.MVar (newEmptyMVar, putMVar, takeMVar)
+#endif
 
 -- | Run your app, taking environment and port settings from the
 --   commandline.
@@ -30,6 +39,35 @@ defaultMain load withSite = do
     config <- load
     logger <- makeLogger
     withSite config logger $ run (appPort config)
+
+-- | Run your application continously, listening for SIGINT and exiting
+--   when recieved
+--
+--   > withYourSite :: AppConfig DefaultEnv -> Logger -> (Application -> IO a) -> IO ()
+--   > withYourSite conf logger f = do
+--   >     Settings.withConnectionPool conf $ \p -> do
+--   >         runConnectionPool (runMigration yourMigration) p
+--   >         defaultRunner f $ YourSite conf logger p
+--
+--   TODO: ifdef WINDOWS
+--
+defaultRunner :: (YesodDispatch y y, Yesod y)
+              => (Application -> IO a)
+              -> y -- ^ your foundation type
+              -> IO ()
+defaultRunner f h =
+#ifdef WINDOWS
+    toWaiApp h >>= f >> return ()
+#else
+    do
+        tid <- forkIO $ toWaiApp h >>= f >> return ()
+        flag <- newEmptyMVar
+        _ <- Signal.installHandler Signal.sigINT (Signal.CatchOnce $ do
+            putStrLn "Caught an interrupt"
+            killThread tid
+            putMVar flag ()) Nothing
+        takeMVar flag
+#endif
 
 -- | Run your development app using the provided @'DefaultEnv'@ type
 --
