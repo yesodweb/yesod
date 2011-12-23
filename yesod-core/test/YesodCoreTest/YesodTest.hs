d8e8cd7afed363063cfdf3d5b135394fa54b0095
Greg Weber <greg@gregweber.info>
2011-12-23 08:05:24 -0300

    tests for redirect status

---
+++ yesod-core/test/YesodCoreTest/YesodTest.hs
@@ -0,0 +1,19 @@
+-- this is being re-worked into a general-purpose testing module for Yesod apps
+module YesodCoreTest.YesodTest
+(   yesod
+  , parseRoutes, mkYesod, yesodDispatch, renderRoute, Yesod(..)
+  , redirect
+  , module Network.Wai
+  , module Network.Wai.Test
+  , module Test.Hspec
+  , module Test.Hspec.HUnit
+) where
+
+import Yesod.Core hiding (Request)
+import Network.Wai.Test
+import Network.Wai
+import Test.Hspec
+import Test.Hspec.HUnit()
+
+yesod :: (YesodDispatch y y, Yesod y) => y -> Session a -> IO a
+yesod app f = toWaiApp app >>= runSession f
