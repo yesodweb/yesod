3435be04966122164fc6df60f45720dc8e454875
Greg Weber <greg@gregweber.info>
2012-01-05 17:48:46 -0300

    remove yesod-mega

--- test/main.hs
+++
@@ -1,9 +0,0 @@
-import Test.Hspec
-import qualified YesodCoreTest
-import qualified YesodStaticTest
-
-main :: IO ()
-main = hspecX $ descriptions [
-    concat YesodCoreTest.specs
-  , concat YesodStaticTest.specs
-  ]
