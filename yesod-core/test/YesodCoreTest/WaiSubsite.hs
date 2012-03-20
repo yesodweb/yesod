ca49415938f5155254ee9ff4cfe4232a4eb753b7
Michael Snoyman <michael@snoyman.com>
2012-03-20 17:30:18 +0200

    Added missing test file

---
+++ yesod-core/test/YesodCoreTest/WaiSubsite.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE QuasiQuotes, TemplateHaskell, TypeFamilies, MultiParamTypeClasses, OverloadedStrings #-}
+module YesodCoreTest.WaiSubsite (specs, Widget) where
+
+import YesodCoreTest.YesodTest
+import Yesod.Core
+import qualified Network.HTTP.Types as H
+import Network.Wai
+
+myApp :: Application
+myApp _ = return $ responseLBS H.status200 [("Content-type", "text/plain")] "WAI"
+
+getApp :: a -> WaiSubsite
+getApp _ = WaiSubsite myApp
+
+data Y = Y
+mkYesod "Y" [parseRoutes|
+/ RootR GET
+/sub WaiSubsiteR WaiSubsite getApp
+|]
+
+instance Yesod Y
+
+app :: Session () -> IO ()
+app = yesod Y
+
+getRootR :: Handler ()
+getRootR = return ()
+
+specs :: [Spec]
+specs = describe "WaiSubsite" [
+    it "root" $ app $ do
+      res <- request defaultRequest { pathInfo = [] }
+      assertStatus 200 res
+      assertBodyContains "" res
+
+  , it "subsite" $ app $ do
+      res <- request defaultRequest { pathInfo = ["sub", "foo"] }
+      assertStatus 200 res
+      assertBodyContains "WAI" res
+  ]
