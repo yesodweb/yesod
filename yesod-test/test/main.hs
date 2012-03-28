a3e27a3d57ee2bd33510377f27b567628d7eafb6
Michael Snoyman <michael@snoyman.com>
2012-03-28 08:03:47 +0200

    Started yesod-test test suite (yo dawg...)

---
+++ yesod-test/test/main.hs
@@ -0,0 +1,14 @@
+import Test.HUnit hiding (Test)
+import Test.Hspec.Monadic
+import Test.Hspec.HUnit ()
+
+import Yesod.Test.CssQuery
+
+parseQuery_ = either (error . show) id . parseQuery
+
+main :: IO ()
+main = hspecX $ do
+    describe "CSS selector parsing" $ do
+        it "elements" $ parseQuery_ "strong" @?= [[DeepChildren [ByTagName "strong"]]]
+        it "child elements" $ parseQuery_ "strong > i" @?= [[DeepChildren [ByTagName "strong"], DirectChildren [ByTagName "i"]]]
+        it "comma" $ parseQuery_ "strong.bar, #foo" @?= [[DeepChildren [ByTagName "strong", ByClass "bar"]], [DeepChildren [ById "foo"]]]
