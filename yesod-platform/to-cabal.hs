fb625002e5a17c1f7e95b6c42ad20102db1953a2
Michael Snoyman <michael@snoyman.com>
2012-04-23 15:25:11 +0300

    Fix to-cabal script

--- yesod-platform/to-cabal.hs
+++ yesod-platform/to-cabal.hs
@@ -3,7 +3,7 @@ import Control.Applicative ((<$>))
 
 main = do
     pkgs <- map (intercalate " == ")
-          . filter (\xs -> not $ ["parsec", "text"] `isPrefixOf` xs)
+          . filter (\xs -> not $ any (`isPrefixOf` xs) $ map return ["parsec", "text", "transformers", "mtl"])
           . map words
           . filter (not . null)
           . lines
