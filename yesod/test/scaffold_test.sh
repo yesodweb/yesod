04364dad658d986241266561f91a2f06f50c99b3
Greg Weber <greg@gregweber.info>
2012-05-20 17:15:57 -0700

    test mongodb and yesod-test

--- yesod/test/scaffold_test.sh
+++ yesod/test/scaffold_test.sh
@@ -3,4 +3,4 @@ teardown() { rm -rf foobar; ghc-pkg unregister foobar &>/dev/null; }
 
 test_sqlite()     { ../test/scaffold.sh < ../test/sqlite-input.txt    ; }
 test_postgresql() { ../test/scaffold.sh < ../test/postgresql-input.txt; }
-#test_mongodb()    { ../test/scaffold.sh < ../test/mongodb-input.txt   ; }
+test_mongodb()    { ../test/scaffold.sh < ../test/mongodb-input.txt   ; }
