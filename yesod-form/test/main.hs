128a9bd1d4cbfa9284db0f958631346d43073dc7
Robert <robert.lee@chicago.vc>
2012-07-13 10:08:59 -0500

    Additional tests on 12 am/pm variants.

--- yesod-form/test/main.hs
+++ yesod-form/test/main.hs
@@ -28,7 +28,13 @@ main = hspec $
         , ("1:00:4 am", Left $ MsgInvalidSecond "4")
         , ("0:00", Right $ TimeOfDay 0 0 0)
         , ("12:00am", Right $ TimeOfDay 0 0 0)
+        , ("12:59:59am", Right $ TimeOfDay 0 59 59)
+        , ("12:59:60am", Left $ MsgInvalidSecond "60")
+        , ("12:60:59am", Left $ MsgInvalidMinute "60)
         , ("12:00pm", Right $ TimeOfDay 12 0 0)
+        , ("12:59:59pm", Right $ TimeOfDay 12 59 59)
+        , ("12:59:60pm", Right $ MsgInvalidSecond "60")
+        , ("12:60:59pm", Right $ MsgInvalidMinute "60")
         , ("12:7pm", Left $ MsgInvalidMinute "7")
         , ("23:47", Right $ TimeOfDay 23 47 0)
         ]
