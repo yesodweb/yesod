d739955944476fee18f444aa4ad65b5bdc05b0f5
Michael <michael@snoyman.com>
2012-08-05 09:41:26 +0300

    Expand yesod-platform package blacklist (#392)

--- yesod-platform/to-cabal.hs
+++ yesod-platform/to-cabal.hs
@@ -3,7 +3,7 @@ import Control.Applicative ((<$>))
 
 main = do
     pkgs <- map (intercalate " == ")
-          . filter (\xs -> not $ any (`isPrefixOf` xs) $ map return ["parsec", "text", "transformers", "mtl", "HUnit", "QuickCheck", "binary"])
+          . filter (\xs -> not $ any (`isPrefixOf` xs) $ map return ["parsec", "text", "transformers", "mtl", "HUnit", "QuickCheck", "binary", "zlib", "stm", "regex-compat"])
           . map words
           . filter (not . null)
           . lines
