ac6ab5b4d0fe681a342c52a4eae81e323b0e2408
Michael Snoyman <michael@snoyman.com>
2013-03-15 07:35:14 +0200

    Remove the long-outdated examples

--- yesod-examples/static/yesod/ajax/script.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-  $("#navbar a").click(function(){
-    $.getJSON($(this).attr("href"), {}, function(o){
-      $("h1").html(o.name);
-      $("article").html(o.content);
-    });
-    return false;
-  });
-});
