f066e66053aaa617c547c31432923b83ef1d6502
Michael Snoyman <michael@snoyman.com>
2013-03-20 15:06:01 +0200

    Streaming Persistent

---
+++ yesod-persistent/test/Yesod/PersistSpec.hs
@@ -0,0 +1,63 @@
+{-# LANGUAGE OverloadedStrings, TemplateHaskell, QuasiQuotes, TypeFamilies #-}
+{-# LANGUAGE EmptyDataDecls, FlexibleContexts, GADTs #-}
+module Yesod.PersistSpec where
+
+import Test.Hspec
+import Database.Persist.Sqlite
+import Network.Wai.Test
+import Yesod.Core
+import Database.Persist.Store
+import Data.Conduit
+import Blaze.ByteString.Builder.Char.Utf8 (fromText)
+import Yesod.Persist
+import qualified Data.Conduit.List as CL
+import Data.Text (Text)
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persist|
+Person
+    name Text
+|]
+
+data App = App
+    { appConfig :: SqliteConf
+    , appPool   :: ConnectionPool
+    }
+
+mkYesod "App" [parseRoutes|
+/ HomeR GET
+|]
+
+instance Yesod App
+instance YesodPersist App where
+    type YesodPersistBackend App = SqlPersist
+    runDB = defaultRunDB appConfig appPool
+instance YesodPersistRunner App where
+    getDBRunner = defaultGetDBRunner appPool
+
+getHomeR :: Handler TypedContent
+getHomeR = do
+    runDB $ do
+        runMigration migrateAll
+        deleteWhere ([] :: [Filter Person])
+        insert $ Person "Charlie"
+        insert $ Person "Alice"
+        insert $ Person "Bob"
+    respondSourceDB typePlain $ selectSource [] [Asc PersonName] $= awaitForever toBuilder
+  where
+    toBuilder (Entity _ (Person name)) = do
+        yield $ Chunk $ fromText name
+        yield $ Chunk $ fromText "\n"
+        yield Flush
+
+test :: String -> Session () -> Spec
+test name session = it name $ do
+    let config = SqliteConf ":memory:" 1
+    pool <- createPoolConfig config
+    app <- toWaiApp $ App config pool
+    runSession session app
+
+spec :: Spec
+spec = test "streaming" $ do
+    sres <- request defaultRequest
+    assertBody "Alice\nBob\nCharlie\n" sres
+    assertStatus 200 sres
