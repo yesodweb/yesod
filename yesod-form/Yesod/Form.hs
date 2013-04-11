fe93881dfecb0ec3bfa59e164befade4a42e545b
Michael Snoyman <michael@snoyman.com>
2013-04-11 15:22:58 +0300

    Fix yesod-form

--- yesod-form/Yesod/Form.hs
+++ yesod-form/Yesod/Form.hs
@@ -3,12 +3,10 @@ module Yesod.Form
     ( module Yesod.Form.Types
     , module Yesod.Form.Functions
     , module Yesod.Form.Fields
-    , module Yesod.Form.Class
     , module Yesod.Form.Input
     ) where
 
 import Yesod.Form.Types
 import Yesod.Form.Functions
 import Yesod.Form.Fields hiding (FormMessage (..))
-import Yesod.Form.Class
 import Yesod.Form.Input
