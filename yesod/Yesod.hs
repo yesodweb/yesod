8e5c419dd5abe40993f4bed25e8e37b74e6661aa
Michael Snoyman <michael@snoyman.com>
2013-06-06 10:05:22 +0300

    Move some Yesod exports to Yesod.Core

--- yesod/Yesod.hs
+++ yesod/Yesod.hs
@@ -6,43 +6,8 @@ module Yesod
       module Yesod.Core
     , module Yesod.Form
     , module Yesod.Persist
-      -- * Commonly referenced functions/datatypes
-    , Application
-      -- * Utilities
-    , showIntegral
-    , readIntegral
-      -- * Hamlet library
-      -- ** Hamlet
-    , hamlet
-    , xhamlet
-    , HtmlUrl
-      -- ** Julius
-    , julius
-    , JavascriptUrl
-    , renderJavascriptUrl
-      -- ** Cassius/Lucius
-    , cassius
-    , lucius
-    , CssUrl
-    , renderCssUrl
     ) where
 
 import Yesod.Core
-import Text.Hamlet
-import Text.Cassius
-import Text.Lucius
-import Text.Julius
-
 import Yesod.Form
 import Yesod.Persist
-
-import Network.Wai
-
-showIntegral :: Integral a => a -> String
-showIntegral x = show (fromIntegral x :: Integer)
-
-readIntegral :: Num a => String -> Maybe a
-readIntegral s =
-    case reads s of
-        (i, _):_ -> Just $ fromInteger i
-        [] -> Nothing
