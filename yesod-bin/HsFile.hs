7e4ef60ae1af4bd2ba755e88c96f82eea67f4d03
Michael Snoyman <michael@snoyman.com>
2014-03-20 12:08:43 +0200

    Drop filesystem-conduit dependency

--- yesod-bin/HsFile.hs
+++ yesod-bin/HsFile.hs
@@ -2,17 +2,31 @@
 module HsFile (mkHsFile) where
 import Text.ProjectTemplate (createTemplate)
 import Data.Conduit 
-    ( ($$), (=$), runResourceT, ResourceT, ConduitM, awaitForever, yield )
+    ( ($$), (=$), runResourceT, ResourceT, ConduitM, awaitForever, yield, Source )
 import qualified Data.Conduit.List as CL
-import Data.Conduit.Filesystem (traverse, sourceFile)
 import Prelude hiding (FilePath)
 import Filesystem.Path ( FilePath )
 import Filesystem.Path.CurrentOS ( encodeString )
+import qualified Filesystem as F
 import qualified Data.ByteString as BS
 import Control.Monad.IO.Class (liftIO)
 
+traverse :: FilePath -> Source (ResourceT IO) FilePath
+traverse dir = do
+    liftIO (F.listDirectory dir) >>= mapM_ go
+  where
+    go fp = do
+        isFile' <- liftIO $ F.isFile fp
+        if isFile'
+            then yield fp
+            else do
+                isDir <- liftIO $ F.isDirectory fp
+                if isDir
+                    then traverse fp
+                    else return ()
+
 mkHsFile :: IO ()
-mkHsFile = runResourceT $ traverse False "."
+mkHsFile = runResourceT $ traverse "."
         $$ readIt
         =$ createTemplate 
         =$ awaitForever (liftIO . BS.putStr)
