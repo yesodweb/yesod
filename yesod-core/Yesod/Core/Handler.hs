615df7bbdfec9dbadb63de464038b1c3bd8e4113
Felipe Lessa <felipe.lessa@gmail.com>
2014-04-10 09:37:18 -0300

    Ditch evaluate for setSession, use bang patterns.

--- yesod-core/Yesod/Core/Handler.hs
+++ yesod-core/Yesod/Core/Handler.hs
@@ -10,6 +10,7 @@
 {-# LANGUAGE TypeFamilies               #-}
 {-# LANGUAGE RankNTypes                 #-}
 {-# LANGUAGE DeriveDataTypeable         #-}
+{-# LANGUAGE BangPatterns               #-}
 ---------------------------------------------------------
 --
 -- Module        : Yesod.Handler
@@ -718,13 +719,11 @@ setSessionBS :: MonadHandler m
              => Text
              -> S.ByteString
              -> m ()
-setSessionBS k bs =
-    liftIO (evaluate $ k `seq` bs) >>= modify . modSession . Map.insert k
+setSessionBS !k !bs = modify . modSession $ Map.insert k bs
 
 -- | Unsets a session variable. See 'setSession'.
 deleteSession :: MonadHandler m => Text -> m ()
-deleteSession k =
-    liftIO (evaluate k) >>= modify . modSession . Map.delete
+deleteSession !k = modify . modSession $ Map.delete k
 
 -- | Clear all session variables.
 --
