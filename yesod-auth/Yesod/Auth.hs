feee2d7de8f7e5b66f8604965216e455c655bd1d
João Cristóvão <jmacristovao@gmail.com>
2014-07-14 10:57:54 +0100

    requireAuthId does not need YesodAuthPersist master

--- yesod-auth/Yesod/Auth.hs
+++ yesod-auth/Yesod/Auth.hs
@@ -56,7 +56,6 @@ import Data.Monoid (Endo)
 import Network.HTTP.Conduit (Manager)
 
 import qualified Network.Wai as W
-import Text.Hamlet (shamlet)
 
 import Yesod.Core
 import Yesod.Persist
@@ -419,7 +418,7 @@ type AuthEntity master = KeyEntity (AuthId master)
 -- authenticated.
 --
 -- Since 1.1.0
-requireAuthId :: YesodAuthPersist master => HandlerT master IO (AuthId master)
+requireAuthId :: YesodAuth master => HandlerT master IO (AuthId master)
 requireAuthId = maybeAuthId >>= maybe redirectLogin return
 
 -- | Similar to 'maybeAuth', but redirects to a login page if user is not
