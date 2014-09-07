587d135f178369e629754f5d8141ae6629beb20c
Michael Snoyman <michael@snoyman.com>
2014-09-07 20:53:57 +0300

    Remove yesod-platform

--- yesod-platform/Yesod/Platform.hs
+++
@@ -1,7 +0,0 @@
--- | This module contains nothing import, it just re-exports @Yesod@. It is
--- provided simply to make this a complete package.
-module Yesod.Platform
-    ( module Yesod
-    ) where
-
-import Yesod
