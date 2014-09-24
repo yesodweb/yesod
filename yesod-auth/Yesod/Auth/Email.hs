01339ad5289eba4719c3ca8f36869b32eecc1a76
Greg Weber <greg@gregweber.info>
2014-09-24 07:47:13 -0700

    add some documentation for Yesod.Auth.Email

--- yesod-auth/Yesod/Auth/Email.hs
+++ yesod-auth/Yesod/Auth/Email.hs
@@ -3,6 +3,22 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE PatternGuards #-}
 {-# LANGUAGE Rank2Types #-}
+-- | A Yesod plugin for Authentication via e-mail
+--
+-- This plugin works out of the box by only setting a few methods on the type class
+-- that tell the plugin how to interoprate with your user data storage (your database).
+-- However, almost everything is customizeable by setting more methods on the type class.
+-- In addition, you can send all the form submissions via JSON and completely control the user's flow.
+-- This is a standare registration e-mail flow
+--
+-- 1) A user registers a new e-mail address, and an e-mail is sent there
+-- 2) The user clicks on the registration link in the e-mail
+--    Note that at this point they are actually logged in (without a password)
+--    That means that when they log out they will need to reset their password
+-- 3) The user sets their password and is redircted to the site.
+-- 4) The user can now
+--    * logout and sign in
+--    * reset their password
 module Yesod.Auth.Email
     ( -- * Plugin
       authEmail
