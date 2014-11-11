89e4e96698dac600dc8c69fed43792f33dc21b95
Simon Hengel <sol@typeful.net>
2014-11-11 22:07:20 +0800

    yesod-test: Fix deprecation warnings

--- yesod-test/Yesod/Test.hs
+++ yesod-test/Yesod/Test.hs
@@ -95,8 +95,7 @@ module Yesod.Test
     , withResponse
     ) where
 
-import qualified Test.Hspec as Hspec
-import qualified Test.Hspec.Core as Hspec
+import qualified Test.Hspec.Core.Spec as Hspec
 import qualified Data.List as DL
 import qualified Data.ByteString.Char8 as BS8
 import Data.ByteString (ByteString)
