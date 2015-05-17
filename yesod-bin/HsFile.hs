814157a018a0a84c111eb052d19cb19486c34f10
Michael Snoyman <michael@snoyman.com>
2015-05-17 11:25:12 +0300

    Fix build for project-template < 0.2

--- yesod-bin/HsFile.hs
+++ yesod-bin/HsFile.hs
@@ -9,14 +9,13 @@ import Control.Monad.Trans.Resource (ResourceT, runResourceT)
 import qualified Data.Conduit.List as CL
 import qualified Data.ByteString as BS
 import Control.Monad.IO.Class (liftIO)
+import Data.String (fromString)
 
 mkHsFile :: IO ()
 mkHsFile = runResourceT $ sourceDirectory "."
         $$ readIt
         =$ createTemplate 
         =$ awaitForever (liftIO . BS.putStr)
-
--- Reads a filepath from upstream and dumps a pair of (filepath, filecontents)
-readIt :: ConduitM FilePath (FilePath, ResourceT IO BS.ByteString) (ResourceT IO) ()
-readIt = CL.map $ \i -> (i, liftIO $ BS.readFile i)
-
+  where
+    -- Reads a filepath from upstream and dumps a pair of (filepath, filecontents)
+    readIt = CL.map $ \i -> (fromString i, liftIO $ BS.readFile i)
