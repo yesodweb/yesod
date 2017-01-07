25723707776d5ae6be54f0fc47921ea3d5877e9f
Cthulhu <cthulhu.den@gmail.com>
2017-01-07 04:24:40 +0300

    Test illustrating incompatibility of requireJsonBody and reading forms

--- yesod-core/test/YesodCoreTest/Json.hs
+++ yesod-core/test/YesodCoreTest/Json.hs
@@ -7,11 +7,17 @@ import qualified Data.Map as Map
 import Network.Wai.Test
 import Data.Text (Text)
 import Data.ByteString.Lazy (ByteString)
+import Network.Wai ( Request(..) )
+import Data.Maybe (fromMaybe, isJust)
+import Data.Aeson (Result)
+import Control.Monad (when, void)
 
 data App = App
 
 mkYesod "App" [parseRoutes|
 / HomeR GET
+/urlencoded UrlEncodedR POST
+/urlencoded/reversed UrlEncodedReversedR POST
 /has-multiple-pieces/#Int/#Int MultiplePiecesR GET
 |]
 
@@ -23,6 +29,19 @@ getHomeR = do
     case Map.lookup ("foo" :: Text) val of
         Nothing -> invalidArgs ["foo not found"]
         Just foo -> return $ RepPlain $ toContent (foo :: Text)
+
+-- | Demonstrates effect of first checking JSON, then trying to access post params
+postUrlEncodedR :: Handler RepPlain
+postUrlEncodedR = do
+    j <- lookupGetParam "parseJSON"
+    when (isJust j) $ void (parseJsonBody :: Handler (Result Text))
+    RepPlain . toContent . fromMaybe "" <$> lookupPostParam "test"
+
+-- | Demonstrates effect of first checking post params, then trying to parse as JSON
+postUrlEncodedReversedR :: Handler RepPlain
+postUrlEncodedReversedR = do
+    void $ lookupPostParam "test"
+    RepPlain . toContent <$> (requireJsonBody :: Handler Text)
 
 getMultiplePiecesR :: Int -> Int -> Handler ()
 getMultiplePiecesR _ _ = return ()
@@ -40,6 +59,30 @@ test name rbody f = it name $ do
             }
         f sres
 
+data Mode = OnlyPost | Both | BothReversed
+
+testUrlEncoded :: Mode
+               -> String
+               -> ByteString
+               -> (SResponse -> Session ())
+               -> Spec
+testUrlEncoded m name rbody f = it name $ do
+    app <- toWaiApp App
+    flip runSession app $ do
+        sres <- srequest SRequest
+            { simpleRequest = setCtMethod $ setPath defaultRequest reqPath
+            , simpleRequestBody = rbody
+            }
+        f sres
+  where
+    setCtMethod req = let ct = ("Content-type", "application/x-www-form-urlencoded")
+                        in req { requestHeaders = ct : requestHeaders req
+                               , requestMethod = "POST"
+                               }
+    reqPath = case m of OnlyPost     -> "/urlencoded"
+                        Both         -> "/urlencoded?parseJSON=yes"
+                        BothReversed -> "/urlencoded/reversed"
+
 specs :: Spec
 specs = describe "Yesod.Json" $ do
     test "parses valid content" "{\"foo\":\"bar\"}" $ \sres -> do
@@ -50,3 +93,12 @@ specs = describe "Yesod.Json" $ do
     test "400 for bad structure" "{\"foo2\":\"bar\"}" $ \sres -> do
         assertStatus 400 sres
         assertBodyContains "foo not found" sres
+    testUrlEncoded OnlyPost "post parameters are available" "test=val" $ \sres -> do
+        assertStatus 200 sres
+        assertBody "val" sres
+    testUrlEncoded Both "post parameters are available after parsing JSON from body" "test=val" $ \sres -> do
+        assertStatus 200 sres
+        assertBody "val" sres
+    testUrlEncoded BothReversed "JSON in available after querying post parameters" "\"val\"" $ \sres -> do
+        assertStatus 200 sres
+        assertBody "val" sres
