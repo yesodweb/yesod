2c59cb7dcdc837d4f60a763998b9e3e69d204ad4
Alex Greif <agreif@users.noreply.github.com>
2017-11-08 13:36:39 +0100

    extend docs of defaultMaybeAuthId (#1453)
    * extend docs of defaultMaybeAuthId
    
    make more explicite that on each call a database access is done. This can be of relevance and sometimes redundant with other Handler functionality
    
    * Update Auth.hs

--- yesod-auth/Yesod/Auth.hs
+++ yesod-auth/Yesod/Auth.hs
@@ -243,7 +243,8 @@ credsKey = "_ID"
 -- | Retrieves user credentials from the session, if user is authenticated.
 --
 -- This function does /not/ confirm that the credentials are valid, see
--- 'maybeAuthIdRaw' for more information.
+-- 'maybeAuthIdRaw' for more information. The first call in a request
+-- does a database request to make sure that the account is still in the database.
 --
 -- Since 1.1.2
 defaultMaybeAuthId
