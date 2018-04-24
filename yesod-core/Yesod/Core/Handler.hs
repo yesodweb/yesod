e664ae2e0efb3e0da58db4315f8207ddebd12b38
ncaq <ncaq@ncaq.net>
2018-04-24 12:55:55 +0900

    changed: use foldC

--- yesod-core/Yesod/Core/Handler.hs
+++ yesod-core/Yesod/Core/Handler.hs
@@ -254,7 +254,7 @@ import           Data.CaseInsensitive (CI, original)
 import qualified Data.Conduit.List as CL
 import           Control.Monad.Trans.Resource  (MonadResource, InternalState, runResourceT, withInternalState, getInternalState, liftResourceT, resourceForkIO)
 import qualified System.PosixCompat.Files as PC
-import           Conduit ((.|), runConduit, sinkLazy)
+import           Conduit ((.|), runConduit, foldC)
 import           Data.Conduit (ConduitT, transPipe, Flush (Flush), yield, Void)
 import qualified Yesod.Core.TypeCache as Cache
 import qualified Data.Word8 as W8
@@ -1392,7 +1392,7 @@ fileSource = transPipe liftResourceT . fileSourceRaw
 --
 -- @since 1.6.5
 fileSourceByteString :: MonadResource m => FileInfo -> m S.ByteString
-fileSourceByteString fileInfo = L.toStrict <$> runConduit (fileSource fileInfo .| sinkLazy)
+fileSourceByteString fileInfo = runConduit $ fileSource fileInfo .| foldC
 
 -- | Provide a pure value for the response body.
 --
