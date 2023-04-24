fdfac8de6f270f2eefaf44b90cae766bbca3c936
ciukstar <ciukstar@yahoo.com>
2023-04-24 17:16:08 +0300

    Change function name from 'englishFormMessage' to 'romanianFormMessage' in module 'Yesod.Form.I18n.Romanian'

--- yesod-form/Yesod/Form/I18n/Romanian.hs
+++ yesod-form/Yesod/Form/I18n/Romanian.hs
@@ -5,27 +5,26 @@ import Yesod.Form.Types (FormMessage (..))
 import Data.Monoid (mappend)
 import Data.Text (Text)
 
--- |
--- Romanian translation
+-- | Romanian translation
 --
 -- @since 1.7.5
-englishFormMessage :: FormMessage -> Text
-englishFormMessage (MsgInvalidInteger t) = "Număr întreg nevalid: " `Data.Monoid.mappend` t
-englishFormMessage (MsgInvalidNumber t) = "Număr nevalid: " `mappend` t
-englishFormMessage (MsgInvalidEntry t) = "Valoare nevalidă: " `mappend` t
-englishFormMessage MsgInvalidTimeFormat = "Oră nevalidă. Formatul necesar este HH:MM[:SS]"
-englishFormMessage MsgInvalidDay = "Dată nevalidă. Formatul necesar este AAAA-LL-ZZ"
-englishFormMessage (MsgInvalidUrl t) = "Adresă URL nevalidă: " `mappend` t
-englishFormMessage (MsgInvalidEmail t) = "Adresă de e-mail nevalidă: " `mappend` t
-englishFormMessage (MsgInvalidHour t) = "Oră nevalidă: " `mappend` t
-englishFormMessage (MsgInvalidMinute t) = "Minut nevalid: " `mappend` t
-englishFormMessage (MsgInvalidSecond t) = "Secundă nevalidă: " `mappend` t
-englishFormMessage MsgCsrfWarning = "Ca protecție împotriva atacurilor CSRF, vă rugăm să confirmați trimiterea formularului."
-englishFormMessage MsgValueRequired = "Câmp obligatoriu"
-englishFormMessage (MsgInputNotFound t) = "Valoare inexistentă: " `mappend` t
-englishFormMessage MsgSelectNone = "<Niciuna>"
-englishFormMessage (MsgInvalidBool t) = "Valoare booleană nevalidă: " `mappend` t
-englishFormMessage MsgBoolYes = "Da"
-englishFormMessage MsgBoolNo = "Nu"
-englishFormMessage MsgDelete = "Șterge?"
-englishFormMessage (MsgInvalidHexColorFormat t) = "Culoarea nevalidă. Formatul necesar este #rrggbb în hexazecimal: " `mappend` t
+romanianFormMessage :: FormMessage -> Text
+romanianFormMessage (MsgInvalidInteger t) = "Număr întreg nevalid: " `Data.Monoid.mappend` t
+romanianFormMessage (MsgInvalidNumber t) = "Număr nevalid: " `mappend` t
+romanianFormMessage (MsgInvalidEntry t) = "Valoare nevalidă: " `mappend` t
+romanianFormMessage MsgInvalidTimeFormat = "Oră nevalidă. Formatul necesar este HH:MM[:SS]"
+romanianFormMessage MsgInvalidDay = "Dată nevalidă. Formatul necesar este AAAA-LL-ZZ"
+romanianFormMessage (MsgInvalidUrl t) = "Adresă URL nevalidă: " `mappend` t
+romanianFormMessage (MsgInvalidEmail t) = "Adresă de e-mail nevalidă: " `mappend` t
+romanianFormMessage (MsgInvalidHour t) = "Oră nevalidă: " `mappend` t
+romanianFormMessage (MsgInvalidMinute t) = "Minut nevalid: " `mappend` t
+romanianFormMessage (MsgInvalidSecond t) = "Secundă nevalidă: " `mappend` t
+romanianFormMessage MsgCsrfWarning = "Ca protecție împotriva atacurilor CSRF, vă rugăm să confirmați trimiterea formularului."
+romanianFormMessage MsgValueRequired = "Câmp obligatoriu"
+romanianFormMessage (MsgInputNotFound t) = "Valoare inexistentă: " `mappend` t
+romanianFormMessage MsgSelectNone = "<Niciuna>"
+romanianFormMessage (MsgInvalidBool t) = "Valoare booleană nevalidă: " `mappend` t
+romanianFormMessage MsgBoolYes = "Da"
+romanianFormMessage MsgBoolNo = "Nu"
+romanianFormMessage MsgDelete = "Șterge?"
+romanianFormMessage (MsgInvalidHexColorFormat t) = "Culoarea nevalidă. Formatul necesar este #rrggbb în hexazecimal: " `mappend` t
