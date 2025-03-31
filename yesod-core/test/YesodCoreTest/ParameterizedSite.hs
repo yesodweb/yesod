0b0203958e8312fa3d08027b4c7380d129b251fa
Benjamin McRae <benjamin@supercede.com>
2025-03-31 11:22:06 +0100

    add a new test to assert that the new format for subroutes compiles and works as expected

--- yesod-core/test/YesodCoreTest/ParameterizedSite.hs
+++ yesod-core/test/YesodCoreTest/ParameterizedSite.hs
@@ -5,14 +5,20 @@ module YesodCoreTest.ParameterizedSite
     ) where
 
 import Data.ByteString.Lazy (ByteString)
-import Network.Wai.Test (runSession, request, defaultRequest, assertBodyContains)
+import Network.Wai.Test (runSession, request, defaultRequest, assertBodyContains, setPath)
 import Test.Hspec (Spec, describe, it)
-import Yesod.Core (YesodDispatch)
+import Yesod.Core (YesodDispatch, renderRoute, Route)
 import Yesod.Core.Dispatch (toWaiApp)
+import Data.Traversable
+import Data.Foldable
+import Data.Text.Encoding
+import Data.Text as T
+import Control.Monad.IO.Class
 
 import YesodCoreTest.ParameterizedSite.PolyAny (PolyAny (..))
 import YesodCoreTest.ParameterizedSite.PolyShow (PolyShow (..))
 import YesodCoreTest.ParameterizedSite.Compat (Compat (..))
+import qualified YesodCoreTest.ParameterizedSite.SubRoute as SR
 
 -- These are actually tests for template haskell. So if it compiles, it works
 parameterizedSiteTest :: Spec
@@ -20,6 +26,7 @@ parameterizedSiteTest = describe "Polymorphic Yesod sites" $ do
     it "Polymorphic unconstrained stub" $ runStub (PolyAny ())
     it "Polymorphic stub with Show" $ runStub' "1337" (PolyShow (1337 :: Int))
     it "Polymorphic unconstrained stub, old-style" $ runStub (Compat () ())
+    it "Polymorphic stub, sub routes" $ runStubAgainst [(SR.HomeR 456, ["Stub", "123", "456"]), (SR.EditorR (SR.AwayR 789), ["Stub", "123", "789"])] (SR.SubRoute (123 :: Int))
 
 runStub :: YesodDispatch a => a -> IO ()
 runStub stub =
@@ -35,4 +42,15 @@ runStub' body stub =
             res <- request defaultRequest
             assertBodyContains "Stub" res
             assertBodyContains body res
+    in toWaiApp stub >>= runSession actions
+
+runStubAgainst :: YesodDispatch a => [(Route a, [ByteString])] -> a -> IO ()
+runStubAgainst urlBodies stub =
+    let actions = do
+            for_ urlBodies $ \(url, bodies) -> do
+                let (nonQuery, _query) = renderRoute url
+                let render = T.intercalate "/" nonQuery
+                let req = setPath defaultRequest (encodeUtf8 render)
+                res <- request req
+                for bodies (`assertBodyContains` res)
     in toWaiApp stub >>= runSession actions
---
+++ yesod-core/test/YesodCoreTest/ParameterizedSite/SubRoute.hs
@@ -0,0 +1,65 @@
+{-# LANGUAGE
+    TypeFamilies, QuasiQuotes, TemplateHaskell, MultiParamTypeClasses
+  , OverloadedStrings, StandaloneDeriving, FlexibleInstances, FlexibleContexts
+  , ViewPatterns, UndecidableInstances, ConstraintKinds
+  #-}
+module YesodCoreTest.ParameterizedSite.SubRoute
+    ( SubRoute (..)
+    , Route (..)
+    , EditorR (..)
+    ) where
+
+import Yesod.Core
+
+type Constraints p = (Eq (Key p), Show (Key p), Read (Key p), PathPiece (Key p), Show p)
+
+class Constraints p => SiteClass p where
+  type Key p :: *
+
+data SubRoute a v = SubRoute a
+  deriving (Eq, Show, Read)
+
+instance SiteClass Int where
+  type Key Int = Int
+
+mkYesod "(SiteClass p) => SubRoute p v" [parseRoutes|
+/home/#{Key p} HomeR GET
+/editor EditorR:
+  /away/#{Key p} AwayR GET
+|]
+
+{-
+The above generates data structures and instances like the following:
+
+  data Route (SubRoute p v) = HomeR (Key p) | EditorR (EditorR p v)
+
+deriving instance SiteClass p => Eq (Route (SubRoute p v))
+deriving instance SiteClass p => Show (Route (SubRoute p v))
+deriving instance SiteClass p => Read (Route (SubRoute p v))
+data EditorR p v = AwayR (Key p)
+deriving instance SiteClass p => Eq (EditorR p v)
+deriving instance SiteClass p => Show (EditorR p v)
+deriving instance SiteClass p => Read (EditorR p v)
+
+Note that `p` is now threaded through the other data structures.
+-}
+
+instance SiteClass a => Yesod (SubRoute a v)
+
+getHomeR :: SiteClass a => Key a -> HandlerFor (SubRoute a v) Html
+getHomeR key = do
+    SubRoute x <- liftHandler getYesod
+    defaultLayout
+        [whamlet|
+            <p>
+                Stub #{show x} #{show key}
+        |]
+
+getAwayR :: SiteClass a => Key a -> HandlerFor (SubRoute a v) Html
+getAwayR key1 = do
+    SubRoute x <- liftHandler getYesod
+    defaultLayout
+        [whamlet|
+            <p>
+                Stub #{show x} #{show key1} #{show "key2"}
+        |]
