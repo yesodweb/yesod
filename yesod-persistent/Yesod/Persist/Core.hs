886a41cd687994b88402e8eeb376b216b80913ac
Felix Paulusma <felix.paulusma@gmail.com>
2025-06-07 18:16:31 +0200

    yesod-newsfeed/persistent/sitemap: order pragmas

--- yesod-persistent/Yesod/Persist/Core.hs
+++ yesod-persistent/Yesod/Persist/Core.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE ConstraintKinds #-}
-{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 -- | Defines the core functionality of this package. This package is
 -- distinguished from Yesod.Persist in that the latter additionally exports the
