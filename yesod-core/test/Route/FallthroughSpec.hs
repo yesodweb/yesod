d2d8b18ccc39ca76a808706acb24ac9b52d3321c
parsonsmatt <parsonsmatt@gmail.com>
2025-11-19 11:03:29 -0700

    ok it is not working

---
+++ yesod-core/test/Route/FallthroughSpec.hs
@@ -0,0 +1,54 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE ViewPatterns#-}
+
+module Route.FallthroughSpec where
+
+import Network.Wai
+import Network.Wai.Test
+import Yesod.Core
+import Test.Hspec
+import Data.Foldable (for_)
+import Test.Hspec
+import Network.Wai
+import Network.Wai.Test
+import Data.ByteString.Lazy (ByteString)
+import qualified Data.ByteString.Char8 as S8
+import Data.String (IsString)
+import Data.Text (Text)
+import Data.Maybe (fromJust)
+import Data.Monoid (Endo (..))
+import qualified Control.Monad.Trans.Writer    as Writer
+import qualified Data.Set as Set
+import Yesod.Routes.TH
+import Language.Haskell.TH
+
+data App = App
+
+do
+    let resources = [parseRoutesNoCheck|
+
+/ HomeR
+/foo FirstFooR:
+    /   FooIndexR
+/foo SecondFooR:
+    /blah FooBlahR
+    |]
+
+    let opts = setNestedRouteFallthrough True defaultOpts
+        appType = ConT (mkName "App")
+    render <- mkRenderRouteInstanceOpts opts [] [] appType resources
+    parse <- mkParseRouteInstanceOpts opts [] appType resources
+    pure (render <> parse)
+
+
